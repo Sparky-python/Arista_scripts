a3c4c863855e4e7bca55fa3659f0308f798a80e2
Mark Rayson <54187620+Sparky-python@users.noreply.github.com>
2020-03-06 23:16:19 +0000

    Update eos_download.py
    Changed to allow downloading multiple files in one go which also allows to create an image bundle with an older EOS image along with a newer version of TerminAttr. Still need to add in some logic to prevent adding an EOS image which needs a newer TerminAttr swix in addition prior to importing into CVP. Also need to think how to cope with 2 image bundles which both use the same TerminAttr image as the imageUpload.py script will crap out if either image exists in CVP already.

--- eos_download.py
+++ eos_download.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2017, Arista Networks, Inc.
+# Copyright (c) 2020, Arista Networks, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,30 +27,37 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# bugalertUpdate.py
+# eos_download.py
 #
 #    Written by:
 #       Mark Rayson, Arista Networks
 #
 """
 DESCRIPTION
-A Python script for updating the AlertBase.json file on CVP when the CVP cannot access 
-the internet directly but a jump host is available which can access the internet and CVP.
-Script can be run manually, or using a scheduler to automatically check www.arista.com
-for database updates for the CVP Bugalerts feature. A local copy of the AlertBase-CVP.json
-file will be left in the directory with the script, it is recommended to leave this file in
-place as the script will compare this local copy to the latest downloaded one and if they are
-the same then no further action will be taken.
-To learn more about BugAlerts see: https://eos.arista.com/eos-4-17-0f/bug-alerts/
+This script is for situations where your CVP server doesn't have internet access but you 
+have a jump host which can access CVP and has internet connectivity. The script downloads 
+the specified EOS image locally and then uploads to the CVP server and creates an image 
+bundle with the image in. It needs as inputs a valid arista.com profile token, the IP 
+address of your CVP server and the root password along with the image version 
+(e.g. 4.22.3F) and the WebGUI username and password of the CVP server you'd like to upload 
+it with. These can be hardcoded into the script by editing the 'default' values in the 
+parser lines of code or passed as commmand line options.
+
+The script can also simply be used as a quick way to download images from arista.com 
+without having to login to the website with SSO, browse through to find the right image 
+and download through a browser. For this use case only the API token, image version and 
+optional international flag options are used (--i)
+
+
 INSTALLATION
 1. python3 needs to be installed on the jump host
-2. wget https://github.com/coreyhines/Arista/raw/master/bugalertUpdate.py
-3. run the script with.. ./bugalertUpdate.py --api <BUGALERTS TOKEN FROM ARISTA.COM> --cvp <CVP SERVER IP ADDRESS> 
---rootpw <ROOT PASSWORD OF CVP SERVER>
-4. python pip install scp, paramiko
+2. pip3 install scp paramiko tqdm
+3. wget https://github.com/Sparky-python/Arista_scripts/blob/master/eos_download.py
+4. Run the script using the following: .\eos_download.py --api {API TOKEN} --file 
+{EOS VERSION} [--file {TERMINATTR VERSION}] [--cvp {CVP IP ADDRESS} --rootpw {ROOT PASSWORD} --cvp_user 
+{GUI CVP USERNAME} --cvp_passwd {GUI CVP PASSWORD}]
 
-Credit to Corey Hinds for original script which this was based on to update BugAlerts file 
-in CVX
+
 """
 __author__ = 'marayson'
 
@@ -101,6 +108,10 @@ warnings.filterwarnings("ignore")
 parser = argparse.ArgumentParser()
 parser.add_argument('--api', required=True,
                     default='', help='arista.com user API key')
+parser.add_argument('--file', required=True, action='append',
+                    default=[], help='EOS and swix iamges to download, repeat --file option for each file. EOS imag
+es should be in the form 4.22.1F for normal images, 4.22.1F-INT for the international/federal version and TerminAtt
+r-1.7.4 for TerminAttr files')
 parser.add_argument('--cvp', required=False,
                     default='', help='IP address of CVP server')
 parser.add_argument('--rootpw', required=False,
@@ -109,10 +120,6 @@ parser.add_argument('--cvp_user', required=False,
                     default='', help='CVP WebUI Username')
 parser.add_argument('--cvp_passwd', required=False,
                     default='', help='CVP WebUI Password')
-parser.add_argument('--eos', required=True,
-                    default='', help='EOS iamge to download')
-parser.add_argument('--i', required=False, action='store_true',
-                    default=False, help='EOS International/Federal Releases')
 
 args = parser.parse_args()
 
@@ -121,8 +128,7 @@ cvp = args.cvp
 rootpw = args.rootpw
 cvp_user = args.cvp_user
 cvp_passwd = args.cvp_passwd
-eos = args.eos
-i = args.i
+file_list = args.file
 
 creds = (base64.b64encode(api.encode())).decode("utf-8")
 
@@ -139,74 +145,110 @@ folder_tree = (result.json()["data"]["xml"])
 root = ET.fromstring(folder_tree)
 path = ""
 
-if i:
-   z = 1
-   eos_filename = "EOS-" + eos + "-INT.swi"
-else:
-   z = 0
-   eos_filename = "EOS-" + eos + ".swi"
+for image in file_list:
+   if "-INT" in image:
+      z = 1
+      eos_filename = "EOS-" + image + ".swi"
+      image = image.rstrip("-INT")
+   elif "TerminAttr" in image:
+      z = 3
+      eos_filename = image + "-1.swix"
+   else:
+      z = 0
+      eos_filename = "EOS-" + image + ".swi"
 
-if os.path.isfile(eos_filename):
-   print ("\nLocal copy of file already exists")
-else:
-   for child in root[z].iter('dir'):
-      if child.attrib == {'label': "EOS-" + eos}:
-         for grandchild in child.iter('file'):
-            if grandchild.text == (eos_filename):
-               path = grandchild.attrib['path']
+   if os.path.isfile(eos_filename):
+      print ("\nLocal copy of file already exists")
+   else:
+      for child in root[z].iter('dir'):
+         if child.attrib == {'label': "EOS-" + image}:
+            for grandchild in child.iter('file'):
+               if grandchild.text == (eos_filename):
+                  path = grandchild.attrib['path']
+         elif child.attrib == {'label': image} or child.attrib == {'label': image + "-1"}  :
+            print (child.attrib)
+            for grandchild in child.iter('file'):
+               if grandchild.text == (eos_filename):
+                  path = grandchild.attrib['path']
 
 
-   if path == "":
-      print("\nEOS image does not exist.")
-      sys.exit()
-   download_link_url = "https://www.arista.com/custom_data/api/cvp/getDownloadLink/"
-   jsonpost = {'sessionCode': session_code, 'filePath': path}
-   result = requests.post(download_link_url, data=json.dumps(jsonpost))
-   download_link = (result.json()["data"]["url"])
 
-   print(eos_filename + " is currently downloading....")
+      if path == "":
+         print("\nFile " + eos_filename +" does not exist.")
+         sys.exit()
+      download_link_url = "https://www.arista.com/custom_data/api/cvp/getDownloadLink/"
+      jsonpost = {'sessionCode': session_code, 'filePath': path}
+      result = requests.post(download_link_url, data=json.dumps(jsonpost))
+      download_link = (result.json()["data"]["url"])
 
-   def download_file(url, filename):
-       """
-       Helper method handling downloading large files from `url` to `filename`. Returns a pointer to `filename`.
-       """
-       chunkSize = 1024
-       r = requests.get(url, stream=True)
-       with open(filename, 'wb') as f:
-          pbar = tqdm( unit="B", total=int( r.headers['Content-Length'] ), unit_scale=True, unit_divisor=1024 )
-          for chunk in r.iter_content(chunk_size=chunkSize): 
-             if chunk: # filter out keep-alive new chunks
-                pbar.update (len(chunk))
-                f.write(chunk)
-       return filename
+      print(eos_filename + " is currently downloading....")
 
-   download_file (download_link, eos_filename)
- 
+      def download_file(url, filename):
+          """
+          Helper method handling downloading large files from `url` to `filename`. Returns a pointer to `filename`.
+          """
+          chunkSize = 1024
+          r = requests.get(url, stream=True)
+          with open(filename, 'wb') as f:
+             pbar = tqdm( unit="B", total=int( r.headers['Content-Length'] ), unit_scale=True, unit_divisor=1024 )
+             for chunk in r.iter_content(chunk_size=chunkSize): 
+                if chunk: # filter out keep-alive new chunks
+                   pbar.update (len(chunk))
+                   f.write(chunk)
+          return filename
+
+      download_file (download_link, eos_filename)
+
 if cvp != '':
-   print ("\nUploading " + eos_filename + " to CVP")
    t = paramiko.Transport((cvp, 22))
    t.connect(username="root", password=rootpw)
    sftp = paramiko.SFTPClient.from_transport(t)
-   cbk, pbar = tqdmWrapViewBar(ascii=True, unit="B", unit_scale=True)
-   sftp.put(eos_filename, '/root/' + eos_filename, callback=cbk)
-   pbar.close()
+   for image in file_list:
+      if "-INT" in image:
+         z = 1
+         filename = "EOS-" + image + ".swi"
+         image = image.rstrip("-INT")
+         eos_filename = filename
+         eos_bundle = image
+      elif "TerminAttr" in image:
+         z = 3
+         filename = image + "-1.swix"
+         terminattr_filename = filename
+      else:
+         z = 0
+         filename = "EOS-" + image + ".swi"
+         eos_filename = filename
+         eos_bundle = image
+
+      print ("\nUploading " + filename + " to CVP")
+      cbk, pbar = tqdmWrapViewBar(ascii=True, unit="B", unit_scale=True)
+      sftp.put(filename, '/root/' + filename, callback=cbk)
+      pbar.close()
 
    ssh = SSHClient()
    ssh.load_system_host_keys()
    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
    ssh.connect(cvp, username="root", password=rootpw)
+
    print ("\nFile copied to CVP server\nNow importing " + eos_filename + " into HDBase.")
 
-   stdin, stdout, stderr = ssh.exec_command('python /cvpi/tools/imageUpload.py --swi ' + eos_filename + ' --bundle EOS-' + eos + ' --user ' + cvp_user + ' --password ' + cvp_passwd)
+   if eos_filename and terminattr_filename:
+      stdin, stdout, stderr = ssh.exec_command('python /cvpi/tools/imageUpload.py --swi ' + eos_filename + ' --swix
+ ' + terminattr_filename + ' --bundle EOS-' + eos_bundle + ' --user ' + cvp_user + ' --password ' + cvp_passwd)
+   else:
+      stdin, stdout, stderr = ssh.exec_command('python /cvpi/tools/imageUpload.py --swi ' + eos_filename + ' --bund
+le EOS-' + eos_bundle + ' --user ' + cvp_user + ' --password ' + cvp_passwd)
    exit_status = stdout.channel.recv_exit_status()
    if exit_status == 0:
       print ("\nUpload complete")
    else:
       print ("\nFile not uploaded because ")
-      if (stdout.read()).decode("utf-8") == "Connecting to CVP\nImage " + eos_filename + " already exists. Aborting.\n":
+      if (stdout.read()).decode("utf-8") == "Connecting to CVP\nImage " + eos_filename + " already exists. Aborting
+.\n":
          print ("Image already exists in CVP")
+      elif "SWI does not contain a supported TerminAttr version" in (stderr.read()).decode("UTF-8"):
+         print ("SWI does not contain a supported TerminAttr version.")
       else:
-         print ("\nSome other error")
-         print (stdout.read().decode("utf-8"))
+         print ("Some other error")
    if ssh:
       ssh.close()
