60e360cf3cdb648b3e85c495c442b7b2563fa9d7
Mark Rayson <54187620+Sparky-python@users.noreply.github.com>
2021-03-23 16:23:32 +0000

    Update bugalertUpdate.py
    Added support to download the bug alert file only.

--- bugalertUpdate.py
+++ bugalertUpdate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2020, Arista Networks, Inc.
+# Copyright (c) 2021, Arista Networks, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -40,15 +40,16 @@ Script can be run manually, or using a scheduler to automatically check www.aris
 for database updates for the CVP Bugalerts feature. A local copy of the AlertBase-CVP.json
 file will be left in the directory with the script, it is recommended to leave this file in
 place as the script will compare this local copy to the latest downloaded one and if they are
-the same then no further action will be taken.
+the same then no further action will be taken. The script can also be used to just download 
+the latest version of the file without uploading to CVP using the --downloadonly option.
 To learn more about BugAlerts see: https://eos.arista.com/eos-4-17-0f/bug-alerts/
 
 INSTALLATION
 1. python3 needs to be installed on the jump host
 2. pip3 install scp paramiko
 3. wget https://github.com/Sparky-python/Arista_scripts/blob/master/bugalertUpdate.py
-4. run the script with.. ./bugalertUpdate.py --api <BUGALERTS TOKEN FROM ARISTA.COM> --cvp 
-<CVP SERVER IP ADDRESS> --rootpw <ROOT PASSWORD OF CVP SERVER>
+4. run the script with.. ./bugalertUpdate.py --api <BUGALERTS TOKEN FROM ARISTA.COM> [--cvp 
+<CVP SERVER IP ADDRESS> --rootpw <ROOT PASSWORD OF CVP SERVER>] [--downloadonly]
 
 Credit to Corey Hinds for original script which this was based on to update BugAlerts file 
 in CVX
@@ -77,15 +78,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--api', required=False,
                     default='2beb105836a4c44b942eed4666d0cd48', help='arista.com user API key')
 parser.add_argument('--cvp', required=False,
-                    default='192.168.32.10', help='IP address of CVP server')
+                    default='', help='IP address of CVP server')
 parser.add_argument('--rootpw', required=False,
-                    default='Arista123', help='Root password of CVP server')
+                    default='', help='Root password of CVP server')
+parser.add_argument('--downloadonly', required=False, action='store_true',
+                    default='', help='Option to only download the alertbase.json file')
+parser.add_argument('--eos', required=False,
+                    default='', help="Version of EOS to check for CVE's against")
 
 args = parser.parse_args()
 
 api = args.api
 cvp = args.cvp
 rootpw = args.rootpw
+downloadonly = args.downloadonly
+eos = args.eos
 
 
 creds = (base64.b64encode(api.encode())).decode("utf-8")
@@ -104,21 +111,22 @@ split_web_data = web_data_final.splitlines()
 alertBaseFile = 'AlertBase-CVP.json'
 logFile = 'bugalertUpdate.log'
 
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(cvp, username="root", password=rootpw)
-ssh_shell = ssh.invoke_shell()
-ssh_shell.send("rpm -qi cvp-base\n")
-time.sleep(2)
-output = ssh_shell.recv(8000)
-output = output.decode("utf-8")
-output = remove_last_line_from_string(output)
-output = remove_last_line_from_string(output)
-output = remove_last_line_from_string(output)
-output = output[(output.index('Name')):]
-Dict = dict((x.strip(), y.strip()) for x, y in (element.split(': ') for element in output.split('\r\n')))
-cvp_main_version = Dict["Version"][0:4]
+if not downloadonly:
+   ssh = SSHClient()
+   ssh.load_system_host_keys()
+   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+   ssh.connect(cvp, username="root", password=rootpw)
+   ssh_shell = ssh.invoke_shell()
+   ssh_shell.send("rpm -qi cvp-base\n")
+   time.sleep(2)
+   output = ssh_shell.recv(8000)
+   output = output.decode("utf-8")
+   output = remove_last_line_from_string(output)
+   output = remove_last_line_from_string(output)
+   output = remove_last_line_from_string(output)
+   output = output[(output.index('Name')):]
+   Dict = dict((x.strip(), y.strip()) for x, y in (element.split(': ') for element in output.split('\r\n')))
+   cvp_main_version = Dict["Version"][0:4]
 
 log = open(logFile, 'a+')
 log.write("\nTimestamp  --  " + (datetime.now().strftime("%m/%d/%Y, %H:%M:%S")) + "\n" + "===================================\n")
@@ -138,22 +146,23 @@ if os.path.isfile(alertBaseFile):
       alertdbfile.close()
       log.write('\n Bug Alert Database successfully created and imported\n')
 
-      ssh = SSHClient()
-      ssh.load_system_host_keys()
-      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      ssh.connect(cvp, username="root", password=rootpw)
-      scp = SCPClient(ssh.get_transport())
-      scp.put('AlertBase-CVP.json')
-      stdin, stdout, stderr = ssh.exec_command('chmod 644 AlertBase-CVP.json')
-      if cvp_main_version == "2020":
-         stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/bugalerts/AlertBase.json')
-      elif (cvp_main_version == "2018") or (cvp_main_version == "2019"): 
-         stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/aeris/bugalerts/AlertBase.json')
-      else:
-         log.write('\n This version of CVP is not supported by this script')
-         sys.exit()
-      stdin, stdout, stderr = ssh.exec_command('su cvp')
-      stdin, stdout, stderr = ssh.exec_command('cvpi stop bugalerts-update && cvp start bugalerts-update')
+      if not downloadonly:
+         ssh = SSHClient()
+         ssh.load_system_host_keys()
+         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+         ssh.connect(cvp, username="root", password=rootpw)
+         scp = SCPClient(ssh.get_transport())
+         scp.put('AlertBase-CVP.json')
+         stdin, stdout, stderr = ssh.exec_command('chmod 644 AlertBase-CVP.json')
+         if cvp_main_version == "2020":
+            stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/bugalerts/AlertBase.json')
+         elif (cvp_main_version == "2018") or (cvp_main_version == "2019"): 
+            stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/aeris/bugalerts/AlertBase.json')
+         else:
+            log.write('\n This version of CVP is not supported by this script')
+            sys.exit()
+         stdin, stdout, stderr = ssh.exec_command('su cvp')
+         stdin, stdout, stderr = ssh.exec_command('cvpi stop bugalerts-update && cvp start bugalerts-update')
    else:
       log.write('\n No updates to Bug Alert Database file.\n')
 else:
@@ -163,15 +172,17 @@ else:
    alertdbfile.close()
    log.write('\n Bug Alert Database successfully created and imported\n')
 
-   ssh = SSHClient()
-   ssh.load_system_host_keys()
-   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-   ssh.connect(cvp, username="root", password=rootpw)
-   scp = SCPClient(ssh.get_transport())
-   scp.put('AlertBase-CVP.json')
-   stdin, stdout, stderr = ssh.exec_command('chmod 644 AlertBase-CVP.json')
-   stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/aeris/bugalerts/AlertBase.json')
-   stdin, stdout, stderr = ssh.exec_command('su cvp')
-   stdin, stdout, stderr = ssh.exec_command('cvpi stop bugalerts-update && cvp start bugalerts-update')
+   if not downloadonly:
+      ssh = SSHClient()
+      ssh.load_system_host_keys()
+      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      ssh.connect(cvp, username="root", password=rootpw)
+      scp = SCPClient(ssh.get_transport())
+      scp.put('AlertBase-CVP.json')
+      stdin, stdout, stderr = ssh.exec_command('chmod 644 AlertBase-CVP.json')
+      stdin, stdout, stderr = ssh.exec_command('mv -f AlertBase-CVP.json /cvpi/apps/aeris/bugalerts/AlertBase.json')
+      stdin, stdout, stderr = ssh.exec_command('su cvp')
+      stdin, stdout, stderr = ssh.exec_command('cvpi stop bugalerts-update && cvp start bugalerts-update')
    
 log.close()
+
